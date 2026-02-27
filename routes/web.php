1a074628e6c00c3d91783707d74047f7c92f1646
Inoussa <inoussa.zerbo@africasys.com>
2026-02-27 13:36:00 +0000

    fix: redirect home route to dashboard instead of rendering welcome page

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return redirect()->route('dashboard');
 })->name('home');
 
 Route::get('dashboard', DashboardController::class)
