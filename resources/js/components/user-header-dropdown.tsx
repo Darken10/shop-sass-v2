25f2a4af290c3ea6e83868f657cfc9d46c2e2045
Inoussa <inoussa.zerbo@africasys.com>
2026-02-28 09:32:31 +0000

    feat: replace UserInfo with Avatar component in user header dropdown

--- resources/js/components/user-header-dropdown.tsx
+++ resources/js/components/user-header-dropdown.tsx
@@ -1,6 +1,7 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { UserInfo } from '@/components/user-info';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { UserMenuContent } from '@/components/user-menu-content';
+import { useInitials } from '@/hooks/use-initials';
 import type { User } from '@/types';
 
 type Props = {
@@ -8,11 +9,18 @@ type Props = {
 };
 
 export function UserHeaderDropdown({ user }: Props) {
+    const getInitials = useInitials();
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <button className="flex items-center rounded-full focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
-                    <UserInfo user={user} />
+                <button className="rounded-full focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
+                    <Avatar className="h-8 w-8 overflow-hidden rounded-full">
+                        <AvatarImage src={user.avatar} alt={user.name} />
+                        <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                            {getInitials(user.name)}
+                        </AvatarFallback>
+                    </Avatar>
                 </button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="min-w-56 rounded-lg" align="end" side="bottom">
